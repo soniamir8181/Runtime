0ed5d7fe7e210250acd4f55c80492c59f764ae20
soniamir8181 <67355565+soniamir8181@users.noreply.github.com>
2020-06-24 01:16:45 -0400

    Update Practice.java
    int added

--- src/test/java/com/practice/Practice.java
+++ src/test/java/com/practice/Practice.java
@@ -3,6 +3,14 @@ package com.practice;
 public class Practice {
 
 	public static void main(String[] args) {
+		
+		int a = 2;
+		int b = 5;
+		int c = a+b;
+		
+		
+System.out.println(c);
+		
 System.out.println("hello");
 System.out.println("hi me");
 	}
